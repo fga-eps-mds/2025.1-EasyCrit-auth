768a475798dfdcdb943e505d1ad12cebb2145044
Luara Cristiana <cristianaluara@gmail.com>
2025-05-19 00:22:21 -0300

    create user post route

--- app/database/database.py
+++ app/database/database.py
@@ -6,8 +6,7 @@ import os
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(
-  DATABASE_URL,
-  connect_args={'check_same_thread': False},
+  DATABASE_URL
 )
 SessionLocal = sessionmaker(
   autocommit=False,
--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from app.schemas import UserSchema
+from app.models import User
+from app.database.database import get_db, create_tables
 
 app = FastAPI()
 
@@ -14,7 +18,24 @@ app.add_middleware(
   allow_headers=['*'],
 )
 
+# Criar tabelas ao iniciar a aplicação
+create_tables()
 
 @app.get('/')
 def read_root():
   return {'message': 'Bem vindo à API de autenticação!'}
+
+# Cria um novo usuário
+@app.post('/users') 
+def create_user(user: UserSchema, db: Session = Depends(get_db)):
+  db_user = User(
+    username=user.username,
+    email=user.email,
+    password=user.password,
+  )
+  db.add(db_user)
+  db.commit()
+  db.refresh(db_user)
+  return db_user
+
+
