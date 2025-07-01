30bbbc577b3659b04f8ea77dc3a7b307584e02b0
Matheus Pimentel <matheusp.eng@gmail.com>
2025-06-30 23:42:08 -0300

    hotfix: fix .env variable pulling

--- app/database/database.py
+++ app/database/database.py
@@ -1,10 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from string import Template
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DB_USER = os.getenv('USER')
+DB_PASSWORD = os.getenv('PASSWORD')
+DB_PORT = os.getenv('DB_PORT')
+HOST = os.getenv('ENV')
+if not HOST == 'production':
+    HOST = 'localhost'
 
-engine = create_engine(DATABASE_URL)
+DB_URL = Template('postgresql+psycopg2://$user:$password@$host:$port/easycrit')
+
+connString = DB_URL.safe_substitute(user=DB_USER, password=DB_PASSWORD, host=HOST, port=DB_PORT)
+engine = create_engine(connString)
+
 SessionLocal = sessionmaker(
   autocommit=False,
   autoflush=False,
@@ -13,14 +23,12 @@ SessionLocal = sessionmaker(
 
 Base = declarative_base()
 
-
 def get_db():
   db = SessionLocal()
   try:
     yield db
   finally:
     db.close()
-
 
 def create_tables():
   Base.metadata.create_all(bind=engine)
